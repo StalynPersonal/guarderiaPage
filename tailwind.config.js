427b67f2ac2375dded3c57e306dd8b27ee02681f
Stalyn Contreras <ing.stalyn@gmail.com>
2026-05-24 18:57:56 -0400

    cambio de directorio del index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.html", "./js/**/*.js"],
+  content: ["./index.html", "./src/**/*.html", "./js/**/*.js"],
   theme: {
     extend: {
       colors: {
